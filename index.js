fe479bd0aa148c76f3189877678696d79c07ecb1
katrinemarku <katrine.marku@studentfr.ch>
2022-06-07 09:51:04 +0200

    footer modification : added an accordion list for the selling adresses
    took the code from a previous project (16-Typoweb) and changed the styles so it suits my website

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var count = 0;
 var directions = {
   prev: 0,
-  next: 1
-}
+  next: 1,
+};
 
-var prevButton = document.querySelector(".slider-navigation button:first-child");
+var prevButton = document.querySelector(
+  ".slider-navigation button:first-child"
+);
 var nextButton = document.querySelector(".slider-navigation button:last-child");
 var sliders = document.querySelectorAll(".slider-content figure");
 
@@ -19,14 +21,14 @@ function onNavigationClick(e) {
   var currentButton = e.target;
   var index = getElementIndex(e.target);
   var controlledCount = countController(index);
-  
+
   var oldSlideItem = sliders[controlledCount.old];
   var newSlideItem = sliders[controlledCount.new];
-  
+
   oldSlideItem.classList.remove("show");
   currentButton.classList.add("disabled");
-  
-  var showNextSliderItemInterval = setInterval(function() {
+
+  var showNextSliderItemInterval = setInterval(function () {
     newSlideItem.classList.add("show");
     currentButton.classList.remove("disabled");
     clearInterval(showNextSliderItemInterval);
@@ -36,20 +38,41 @@ function onNavigationClick(e) {
 function countController(directionIndex) {
   var result = { new: 0, old: count };
   var max = sliders.length;
-  
-  if(directionIndex === directions.next) {
-    count = count === max - 1 ? 0 : (count + 1);
+
+  if (directionIndex === directions.next) {
+    count = count === max - 1 ? 0 : count + 1;
   }
-  
-  if(directionIndex === directions.prev) {
-    count = count === 0 ? (max - 1) : (count - 1);
+
+  if (directionIndex === directions.prev) {
+    count = count === 0 ? max - 1 : count - 1;
   }
-  
+
   result.new = count;
-  
+
   return result;
 }
 
 function getElementIndex(element) {
   return Array.from(element.parentElement.children).indexOf(element);
 }
+
+const accordion = document.querySelector(".accordion");
+const items = accordion.querySelectorAll("li");
+const questions = accordion.querySelectorAll(".question");
+
+function toggleAccordion() {
+  const thisItem = this.parentNode;
+
+  items.forEach((item) => {
+    if (thisItem == item) {
+      thisItem.classList.toggle("open");
+      return;
+    }
+
+    item.classList.remove("open");
+  });
+}
+
+questions.forEach((question) =>
+  question.addEventListener("click", toggleAccordion)
+);
